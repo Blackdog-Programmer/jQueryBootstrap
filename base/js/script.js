c2fc15cc2c24c41d801f59f1a86d20df635414fd
blackdogcode <blackdogcode@gmail.com>
2020-03-08 19:22:56 +0900

    fade effects

--- base/js/script.js
+++ base/js/script.js
@@ -1,6 +1,11 @@
 $(function() {
   // jQuery goes here...
 
-  // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  $(".red-box").fadeOut(2000);
+  $(".green-box").fadeOut(2000);
+
+  $(".blue-box").fadeTo(2000, 0.6);
+
+  $(".red-box").fadeToggle();
+  $(".green-box").fadeToggle();
 });
